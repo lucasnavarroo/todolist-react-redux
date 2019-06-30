eb7cfd0704ea05e105a516d5ed5af1b17c1ebc63
Lucas Navarro <lucasnavarro979@gmail.com>
2019-06-30 15:14:23 -0300

    repositorio duplicado

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 import api from "../../services/api";
 
 import { Creators as FavoriteActions } from "../ducks/favorites";
@@ -6,16 +6,26 @@ import { Creators as FavoriteActions } from "../ducks/favorites";
 export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
-    console.log(data);
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url
-    };
 
-    yield put(FavoriteActions.addFavoritesSuccess(repositoryData));
+    const isDuplicated = yield select(state =>
+      state.favorites.data.find(favorite => favorite.id === data.id)
+    );
+
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoritesFailure("repositório duplicado"));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url
+      };
+
+      yield put(FavoriteActions.addFavoritesSuccess(repositoryData));
+    }
   } catch (err) {
-    yield put(FavoriteActions.addFavoritesFailure("Erro ao adicionar repositório"));
+    yield put(
+      FavoriteActions.addFavoritesFailure("Erro ao adicionar repositório")
+    );
   }
 }
